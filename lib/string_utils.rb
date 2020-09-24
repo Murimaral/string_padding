f8e8261a0d1662e2ba7b221e1558b1c7167d79e1
Murimaral <murimaral@gmail.com>
2020-09-24 17:47:39 -0300

    WIP teste iniciado
    Co-authored-by: Tatiana Oliveira <TatianaRo>

--- lib/string_utils.rb
+++ lib/string_utils.rb
@@ -1,5 +1,8 @@
 class StringUtils
   def padding(frase, padding_array)
+     
+     "#{"\n"*padding_array[0]}#{' '*padding_array[1]}#{frase}#{' '*padding_array[2]}#{"\n"*padding_array[3]}"
+   
 
   end
 end
--- spec/string_utils_spec.rb
+++ spec/string_utils_spec.rb
@@ -9,4 +9,12 @@ describe StringUtils do
                   "\n"
     expect(StringUtils.new.padding(frase, [1, 1, 1, 1])).to eq expectativa
   end
+  it 'Checa se a frase faz padding de 2 em todas as direções' do
+    frase = 'Telefone... Minha casa.'
+
+    expectativa = "\n\n"\
+                  "  Telefone... Minha casa.  "\
+                  "\n\n"
+    expect(StringUtils.new.padding(frase, [2])).to eq expectativa
+  end
 end
